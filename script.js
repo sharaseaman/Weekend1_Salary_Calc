56778808646b08b88af90b7b0f609919c37c1e25
sharaseaman <shara.seaman@gmail.com>
2017-12-17 13:06:37 -0600

    test

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ $(document).ready(onReady);
 
   //global variables
   var formInfo = [];
-  var totalSalary = 0;
+  //var totalSalary = 0;
   var monthlyCost = 0;
 
 function onReady() {
     console.log("jquery ready!");
     
     // event listeners go here
-    $('#submitButton').on('click', addInfo)
+    $('#submitButton').on('click', callsBoth)
+    
     
 }
 
+function callsBoth(){
+    addInfo();
+    debugger;
+    $('#monthlyCost').text(calcSalaries(formInfo));
+    
+
+}
 
 function addInfo(){
     console.log('addInfo click button working');
@@ -23,34 +31,32 @@ function addInfo(){
               ($( '#firstName' ).val(),
               $( '#lastName' ).val(), 
               $( '#idNumber' ).val(), 
-              $( '#jobTitle' ).val(), 
+              $( '#jobTitle' ).val(),  
               $( '#annualSalary' ).val());
-    
+    formInfo.push(newPerson);
 
-    totalSalary += Number(newPerson.annualSalary);
-    monthlyCost = Math.round(totalSalary/12);
-    $('#monthlyCost').text(monthlyCost);
+    // totalSalary += Number(newPerson.annualSalary);
+    // monthlyCost = Math.round(totalSalary/12);
+    // $('#monthlyCost').text(monthlyCost);
 
-       //reset       
-    // $( '#firstName' ).val( '' ); 
-    // $( '#lastName' ).val( '' );
-    // $( '#idNumber' ).val( '' );
-    // $( '#jobTitle' ).val( '' );
-    // $( '#annualSalary' ).val( '' );
+    //    reset       
+    $( '#firstName' ).val( '' ); 
+    $( '#lastName' ).val( '' );
+    $( '#idNumber' ).val( '' );
+    $( '#jobTitle' ).val( '' );
+    $( '#annualSalary' ).val( '' );
 
-     $( '.createClass' ).val( '' );
 
 console.log(formInfo);
 
- // append info to DOM ??????????????????? ?!
-       //$('#displayEmployee').html(monthlyTotals);
+ // append info to DOM
        $('#displayEmployee').append('<p>Name:'+ ' ' + $('#firstName').val() + ' ' + $('#lastName').val() + '' + 
        '<br>ID Number:' + ' ' + $('#idNumber').val() + '' + 
        '<br>Job Title:' + ' ' + $('#jobTitle').val() + '' + 
        '<br>Annual Salary:' + ' ' + '$'+  $('#annualSalary').val() + 
        '<br>Monthly Total Cost for this Employee:' + ' $' + Math.round($('#annualSalary').val() / 12) + '</p>');
    //clears the input fields so a new employee can be added
-       $('.info').val(''); 
+    //    $('.info').val(''); 
 } //end 
 
 
@@ -61,20 +67,21 @@ function Person( firstName, lastName, idNumber, jobTitle, annualSalary ){
     this.idNumber = idNumber;
     this.jobTitle = jobTitle;
     this.annualSalary = annualSalary;   
-    formInfo.push( this);
+    // formInfo.push( this);
 } // end info constructor
 
 
-    //calculate the salaries somehow 
+    //calculate the salaries 
 
 function calcSalaries( formInfo ) {
     // var for total salary
-
+    var totalSalary = 0;
     // loop through array
     for (var i = 0; i < formInfo.length; i++) {
         totalSalary += formInfo[i].annualSalary;
     }
-    monthlyCost = totalSalary / 12;
+    debugger;
+    return totalSalary / 12;
 }
 
     
